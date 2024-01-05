3db454f0c432bd3f2498394e29f0c9869d558023
Sreeshma Tharammal <sreeshmajeenus@gmail.com>
2024-01-05 20:47:41 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
+
+
